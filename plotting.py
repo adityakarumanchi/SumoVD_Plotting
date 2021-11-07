ffa46f49847fa62395ddcf5a57b47cfce9fdae81
karumanchi.1 <karumanchi.1@sim-dnc244509d.coeit.osu.edu>
2021-11-07 15:18:38 -0500

    slight mods, plotted using plot func

--- plotting.py
+++ plotting.py
@@ -17,15 +17,18 @@ def run_xml2csv(filepath):
         os.system("""python %s %s --separator "," """ % (os.path.join(tools, 'xml', 'xml2csv.py'), filepath))
 
 
-def plotstuff(axis, dataset, xlabel, ylabel, style, legendlabel, axeslabels, savename):
+def plotstuff(axis, dataset, xlabel, ylabel, style, legendlabel, axeslabels):#, bLegend, savename):
     axis.plot(dataset[xlabel], dataset[ylabel], style, label=legendlabel)
     if 'x' in axeslabels.keys():
         axis.set_xlabel(axeslabels['x'])
     elif 'y' in axeslabels.keys():
         axis.set_xlabel(axeslabels['y'])
 
-    if len(savename):
-        plt.savefig(fname=savename)
+    # if bLegend:
+    #     plt.legend()
+    #
+    # if len(savename):
+    #     plt.savefig(fname=savename)
 
 
 SumoDataDir = r'/home/karumanchi.1/Downloads/Downtown/'
@@ -41,30 +44,29 @@ col_names = [vd_first_row.iloc[idx] for idx in vddata.columns if idx % 2 == 0]
 retained_cols = [idx for idx in vddata.columns if idx % 2 == 1]
 vddata = vddata.loc[:, retained_cols]
 vddata.columns = col_names
-print(vddata)
-vddata['Yposition'] *= -1
-vddata['Yaw'] *= 180/math.pi
-print(vddata)
+vddata['Xposition'] *= 0.01
+vddata['Yposition'] *= -0.01
+vddata['Yaw'] *= -180/math.pi
 
-# fig1, axs = plt.subplots(3, 1)
-# plot_labels_vddata=[('Time, Xposition'), ('Time, Yposition'), ('Time, Yaw')]
-# ax1.plot(vddata['Time'], vddata['Xposition'] / 100, 'b-', label='VD Output')
-# ax1.plot(sumodata['timestep_time'], sumodata['vehicle_x'], 'r-', label='SUMO')
-# ax2.plot(vddata['Time'], -vddata['Yposition'] / 100, 'b-', label='VD Output')
-# ax2.plot(sumodata['timestep_time'], sumodata['vehicle_y'], 'r-', label='SUMO')
-# ax2.set_xlabel('Time (s)')
-# ax1.set_ylabel('X-Position (m)')
-# ax2.set_ylabel('Y-Position (m)')
-# plt.legend()
+fig1, axs = plt.subplots(3, 1)
+plot_labels_vddata= zip(axs, ['Time' for _ in range(len(axs))], ['Xposition', 'Yposition', 'Yaw'])
+plot_labels_sumodata= zip(axs, ['timestep_time' for _ in range(len(axs))], ['vehicle_x', 'vehicle_y', 'vehicle_angle'])
 
-# plt.savefig(fname='X_and_Y_Pos_vs_Time.pdf')
-#
-# fig2, ax = plt.subplots()
-# ax.plot(sumodata['vehicle_x'], sumodata['vehicle_y'], 'r-', label='SUMO')
-# ax.plot(vddata['Xposition'] / 100, -vddata['Yposition'] / 100, 'b--', label='VD Output')
-# ax.set_xlabel('X-Position (m)')
-# ax.set_ylabel('Y-Position (m)')
-#
-# plt.legend()
-# plt.savefig(fname='X_vs_Y_Pos.pdf')
+idx = 0
+
+for ax, sumox, sumoy in plot_labels_sumodata:
+    plotstuff(ax, sumodata, sumox, sumoy, 'r-', 'SUMO', {})
+
+for ax, vdx, vdy in plot_labels_vddata:
+    plotstuff(ax, vddata, vdx, vdy, 'b-', 'Veh. Dyn.', {'x': 'Time (s)', 'y': vdx})
+
+plt.legend(loc='upper right')
+plt.savefig(fname='Time_vs_Output.pdf')
+
+fig2, ax = plt.subplots()
+plotstuff(ax, sumodata, 'vehicle_x', 'vehicle_y', style='r-', legendlabel='SUMO', axeslabels={'x': 'X-Position (m)', 'y': 'Y-Position (m)'})
+plotstuff(ax, vddata, 'Xposition', 'Yposition', style='b-', legendlabel='Veh. Dyn.', axeslabels={})
+
+plt.legend()
+plt.savefig(fname='X_vs_Y_Pos.pdf')
 # plt.show()
